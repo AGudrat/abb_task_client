06c538e8fc871e1a2f8a6fc92fa9126827a1ce79
Gudrat Abidzada <92165132+AGudrat@users.noreply.github.com>
2024-09-24 11:20:21 +0400

    favicons added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,13 +21,44 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Suspense>
-          <main className="flex flex-col">
-            {children}
-          </main>
+          <main className="flex flex-col">{children}</main>
         </Suspense>
       </body>
     </html>
